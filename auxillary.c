743ef912911d08228b2e2b4b35d5abdab9114894
Blake McBride <blake@arahant.com>
2015-02-22 07:10:36 -0600

    fixed line-endings

--- auxillary.c
+++ auxillary.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
 int getch_(char *vec, char *ch, int *i)
 {
     ch[0] = vec[*i-1];
@@ -11,31 +10,24 @@ int getch_(char *vec, char *ch, int *i)
     ch[3] = ' ';
     return 0;
 }
-
 int putch_(char *vec, char *ch, int *i)
 {
     vec[*i-1] = *ch;
     return 0;
 }
-
-
 int upcase_(char *buff, int *n)
 {
 	int	i, x;
-
 	for (x=i=0 ; i++ < *n ; x+=4)
 		buff[x] = toupper(buff[x]);
 	return 0;
 }
-
 static	FILE	*Logical_units[100];
-
 void	setup()
 {
 	Logical_units[5] = stdin;
 	Logical_units[6] = stdout;
 }
-
 int	f4_open(int lun, char *file, char *mode)
 {
 	if (Logical_units[lun])
@@ -43,7 +35,6 @@ int	f4_open(int lun, char *file, char *mode)
 	Logical_units[lun] = fopen(file, mode);
 	return Logical_units[lun] ? 0 : 1;
 }
-
 int	f4_close(int lun)
 {
 	if (Logical_units[lun]) {
@@ -52,19 +43,14 @@ int	f4_close(int lun)
 	}
 	return 0;
 }
-
-
 static	int	read_status;  /*  1=do read,  2=at eol, 3=at eof  */
-
 void	f4_start_read()
 {
 	read_status = 1;
 }
-
 static	int	read1(FILE *fp)
 {
 	int	c;
-
 	if (read_status == 1) {
 		c = getc(fp);
 		if (c == '\r'  ||  c == '\n') {
@@ -78,13 +64,11 @@ static	int	read1(FILE *fp)
 		c = ' ';
 	return c;
 }
-
 int	f4_read(int lun, int *ci, int n)
 {
 	FILE	*fp = Logical_units[lun];
 	char	*v = (char *) ci;
 	int	c;
-
 	if (read_status == 3)
 		return 1;
 	v[0] = read1(fp);
@@ -99,13 +83,10 @@ int	f4_read(int lun, int *ci, int n)
 	}
 	return 0;
 }
-
-
 int	f4_readu(int lun, int *ci, int n)
 {
 	FILE	*fp = Logical_units[lun];
 	char	*v = (char *) ci;
-
 	v[0] = getc(fp);
 	if (n == 4) {
 		v[1] = getc(fp);
@@ -114,20 +95,16 @@ int	f4_readu(int lun, int *ci, int n)
 	}
 	return 0;
 }
-
 int	f4_rewind(int lun)
 {
 	FILE	*fp = Logical_units[lun];
 	rewind(fp);
 	return 0;
 }
-
-
 int	f4_write(int lun, int *ci, int n)
 {
 	FILE	*fp = Logical_units[lun];
 	char	*v = (char *) ci;
-
 	putc(v[0], fp);
 	if (n == 4) {
 		putc(v[1], fp);
@@ -136,13 +113,9 @@ int	f4_write(int lun, int *ci, int n)
 	}
 	return 0;
 }
-
-
 int	f4_write_lf(int lun)
 {
 	FILE	*fp = Logical_units[lun];
-
 	putc('\n', fp);
 	return 0;
 }
-
